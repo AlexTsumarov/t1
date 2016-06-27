00783da6f459ca2505cdc8301a8f7c245fe21a2f
Alex <Alexandr_Tsumarov1@epam.com>
2016-06-27 12:55:38 +0300

    Update gulp.js

--- gulp.js
+++ gulp.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var scp = require('gulp-scp');
  
 gulp.task('default', function () {
-    gulp.src('*')
+    gulp.src('README.md')
         .pipe(scp({
             host: 'nl-wag-epam01.meteogroup.net',
             user: 'alexandrt',
